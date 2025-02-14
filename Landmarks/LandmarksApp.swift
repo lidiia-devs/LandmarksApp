f9034c4cbe12aa31ac6b6c771971ac77d0d0f7f1
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-02-13 21:03:09 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lidiia Diachkovskaia on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
