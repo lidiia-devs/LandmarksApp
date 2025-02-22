c9ffb6d3f0286155e6fb422867910b868dfc5601
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-02-22 15:43:20 -0500

    #

---
+++ Landmarks/Categories/CategoryHome.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Lidiia Diachkovskaia on 2/21/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List{
+                ForEach(modelData.categories.keys.sorted(), id: \.self) {
+                    key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+            }
+                .navigationTitle("Featured")
+            
+        } detail: {
+            Text("Select a Landmark")
+        }
+    
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -6,12 +6,20 @@
 //
 
 import Foundation
+
 @Observable
 class ModelData {
     
     var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarks, by:
+                    {$0.category.rawValue}
+        )
+    }
+    
 }
-
 
 
 var number: Int = 1
